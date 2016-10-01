03f7bb0fc55db0e3d778b2073a67972221ebbfa2
eduarmreyes <eduar.mreyes@gmail.com>
2016-09-30 23:59:06 -0600

    Navigate through the galery

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function() {
+	$(document).on("ready", function(e) {
+		fnLoadGaleryContent("home");
+	});
+	$(".nav.navbar-nav a").on("click", function(e) {
+		fnLoadGaleryContent($(this).data("content"));
+	});
+	function fnLoadGaleryContent(sOption) {
+		$.ajax({
+			dataType: "html",
+			success: function(data) {
+				$(".galery").html(data);
+			},
+			type: "get",
+			url: "_" + sOption + ".html"
+		});
+	}
+})();
