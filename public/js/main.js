7ac750a6e6a99e21f85863d0b3c890e0040944dc
eduarmreyes <eduar.mreyes@gmail.com>
2016-10-06 07:46:31 -0600

    Show background-size cover - Change just-share image

--- public/js/main.js
+++ public/js/main.js
@@ -72,45 +72,53 @@
 		$scope.color_class = "color-black";
 		$(".fullscreen-bg").fadeOut();
 		$("body").css("background", "url('img/at_your_service_backgroup.png') no-repeat center center fixed");
+		$("body").css("background-size", "cover");
 	}]);
 
 	GalleryApp.controller('BeTheDirectorCtrl', ["$scope", function($scope){
 		$(".fullscreen-bg").fadeOut();
 		$("body").css("background", "url('img/be_the_director_backgroup.png') no-repeat center center fixed");
+		$("body").css("background-size", "cover");
 	}]);
 
 	GalleryApp.controller('ContentManagementCtrl', ["$scope", function($scope){
 		$(".fullscreen-bg").fadeOut();
 		$("body").css("background", "url('img/content_management_backgroup.png') no-repeat center center fixed");
+		$("body").css("background-size", "cover");
 	}]);
 
 	GalleryApp.controller('JustShareCtrl', ["$scope", function($scope){
 		$scope.color_class = "color-black";
 		$(".fullscreen-bg").fadeOut("");
 		$("body").css("background", "url('img/just_share_backgroup.png') no-repeat center center fixed");
+		$("body").css("background-size", "cover");
 	}]);
 
 	GalleryApp.controller('MultipleUsersCtrl', ["$scope", function($scope){
 		$scope.color_class = "color-black";
 		$(".fullscreen-bg").fadeOut("");
 		$("body").css("background", "url('img/multiple_users_backgroup.png') no-repeat center center fixed");
+		$("body").css("background-size", "cover");
 	}]);
 
 	GalleryApp.controller('OurRelationshipsCtrl', ["$scope", function($scope){
 		$scope.color_class = "color-black";
 		$(".fullscreen-bg").fadeOut("");
 		$("body").css("background", "url('img/our_relationships_backgroup.png') no-repeat center center fixed");
+		$("body").css("background-size", "cover");
 	}]);
 
 	GalleryApp.controller('PortfolioCtrl', ["$scope", function($scope){
 		$scope.color_class = "color-black";
 		$(".fullscreen-bg").fadeOut();
 		$("body").css("background", "url('img/portfolio_on_the_go_backgroup.png') no-repeat center center fixed");
+		$("body").css("background-size", "cover");
 	}]);
 
 	GalleryApp.controller('SupportCtrl', ["$scope", function($scope){
 		$(".fullscreen-bg").fadeOut();
 		$("body").css("background", "url('img/support_backgroup.png') no-repeat center center fixed");
+		$("body").css("background-size", "cover");
 	}]);
 
 })();
