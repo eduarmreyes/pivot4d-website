cad011ddf82dd210a1c12cf487664652a7e80a50
eduarmreyes <eduar.mreyes@gmail.com>
2016-10-06 07:00:56 -0600

    Show complete gallery options - Show color depending on view

--- public/js/main.js
+++ public/js/main.js
@@ -11,16 +11,40 @@
 					return "partials/_home.html";
 				}
 			})
-			.when("/our-relationships", {
-				controller:		"OurRelationshipsCtrl",
-				templateUrl: function() {
-					return "partials/_our-relationships.html";
-				}
-			})
 			.when("/at-your-service", {
 				controller:		"AtYourServiceCtrl",
 				templateUrl: function() {
 					return "partials/_at-your-service.html";
+				}
+			})
+			.when("/be-the-director", {
+				controller:		"BeTheDirectorCtrl",
+				templateUrl: function() {
+					return "partials/_be-the-director.html";
+				}
+			})
+			.when("/content-management", {
+				controller:		"ContentManagementCtrl",
+				templateUrl: function() {
+					return "partials/_content-management.html";
+				}
+			})
+			.when("/just-share", {
+				controller:		"JustShareCtrl",
+				templateUrl: function() {
+					return "partials/_just-share.html";
+				}
+			})
+			.when("/multiple-users", {
+				controller:		"MultipleUsersCtrl",
+				templateUrl: function() {
+					return "partials/_multiple-users.html";
+				}
+			})
+			.when("/our-relationships", {
+				controller:		"OurRelationshipsCtrl",
+				templateUrl: function() {
+					return "partials/_our-relationships.html";
 				}
 			})
 			.when("/portfolio", {
@@ -40,26 +64,51 @@
 			});
 	}]);
 
-	GalleryApp.controller('HomeCtrl', [function(){
+	GalleryApp.controller('HomeCtrl', ["$scope", function($scope){
 		$(".fullscreen-bg").fadeIn();
 	}]);
 
-	GalleryApp.controller('OurRelationshipsCtrl', [function(){
-		$(".fullscreen-bg").fadeOut("");
-		$("body").css("background", "url('img/our_relationships_backgroup.png') 0% 0% / cover no-repeat");
-	}]);
-
-	GalleryApp.controller('AtYourServiceCtrl', [function(){
+	GalleryApp.controller('AtYourServiceCtrl', ["$scope", function($scope){
+		$scope.color_class = "color-black";
 		$(".fullscreen-bg").fadeOut();
 		$("body").css("background", "url('img/at_your_service_backgroup.png') 0% 0% / cover no-repeat");
 	}]);
 
-	GalleryApp.controller('PortfolioCtrl', [function(){
+	GalleryApp.controller('BeTheDirectorCtrl', ["$scope", function($scope){
+		$(".fullscreen-bg").fadeOut();
+		$("body").css("background", "url('img/be_the_director_backgroup.png') 0% 0% / cover no-repeat");
+	}]);
+
+	GalleryApp.controller('ContentManagementCtrl', ["$scope", function($scope){
+		$(".fullscreen-bg").fadeOut();
+		$("body").css("background", "url('img/content_management_backgroup.png') 0% 0% / cover no-repeat");
+	}]);
+
+	GalleryApp.controller('JustShareCtrl', ["$scope", function($scope){
+		$scope.color_class = "color-black";
+		$(".fullscreen-bg").fadeOut("");
+		$("body").css("background", "url('img/just_share_backgroup.png') 0% 0% / cover no-repeat");
+	}]);
+
+	GalleryApp.controller('MultipleUsersCtrl', ["$scope", function($scope){
+		$scope.color_class = "color-black";
+		$(".fullscreen-bg").fadeOut("");
+		$("body").css("background", "url('img/multiple_users_backgroup.png') 0% 0% / cover no-repeat");
+	}]);
+
+	GalleryApp.controller('OurRelationshipsCtrl', ["$scope", function($scope){
+		$scope.color_class = "color-black";
+		$(".fullscreen-bg").fadeOut("");
+		$("body").css("background", "url('img/our_relationships_backgroup.png') 0% 0% / cover no-repeat");
+	}]);
+
+	GalleryApp.controller('PortfolioCtrl', ["$scope", function($scope){
+		$scope.color_class = "color-black";
 		$(".fullscreen-bg").fadeOut();
 		$("body").css("background", "url('img/portfolio_on_the_go_backgroup.png') 0% 0% / cover no-repeat");
 	}]);
 
-	GalleryApp.controller('SupportCtrl', [function(){
+	GalleryApp.controller('SupportCtrl', ["$scope", function($scope){
 		$(".fullscreen-bg").fadeOut();
 		$("body").css("background", "url('img/support_backgroup.png') 0% 0% / cover no-repeat");
 	}]);
